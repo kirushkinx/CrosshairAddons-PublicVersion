52571f781f87b2db95ef02dccfb07d455d6f9eb0
Walksy <69202220+Walksy@users.noreply.github.com>
2024-10-18 19:48:33 +0100

    Impl code

---
+++ src/main/java/walksy/crosshairaddons/CrosshairAddons.java
@@ -0,0 +1,14 @@
+package walksy.crosshairaddons;
+
+import net.fabricmc.api.ModInitializer;
+import walksy.crosshairaddons.manager.ConfigManager;
+
+public class CrosshairAddons implements ModInitializer {
+
+
+    @Override
+    public void onInitialize()
+    {
+        ConfigManager.load();
+    }
+}
